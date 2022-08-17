7776e56a50300791cc5d72a8512e4359f0ab36d7
John Johnson Mbeba <john.mbeba@student.moringaschool.com>
2022-08-17 09:18:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr){
+    return arr.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(arr){
+    return arr.slice(arr.length-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (mul) => {
+    return function(val){
+        return mul * val;
+    }
+}
+
+const fareDoubler = (fare) => {
+    return fare * 2;
+}
+
+const fareTripler = (fare) => {
+    return fare * 3;
+}
+
+const selectDifferentDrivers = (arr, fn) => {
+    return fn(arr);
+}
